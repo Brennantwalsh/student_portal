a976268f5f8f8cb938fe7d5f521197111fc76a09
verynear <scott.a.near@gmail.com>
2016-10-31 18:59:20 -0500

    student model

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -2,4 +2,24 @@ class EducationsController < ApplicationController
   def index
     @educations = Education.all
   end
+
+  def show
+    @education = Education.find(params[:id])
+  end
+
+  def edit
+    @education = Education.find(params[:id])
+  end
+
+  def update
+    @id = params[:id]
+    @education = Education.find(params[:id])
+    @education.update({start_date: params[:start_date],
+                       end_date: params[:end_date],
+                       degree: params[:degree],
+                       university_name: params[:university_name],
+                       details: params[:details]
+      })
+    redirect_to "/educations/#{@id}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,29 @@
 Rails.application.routes.draw do
 
-	get '/signup' => 'users#new'
-	post '/users' => 'users#create'
-	  
-	get '/login' => 'sessions#new'
-	post '/login' => 'sessions#create'
-	delete '/logout' => 'sessions#destroy'
+get '/signup' => 'users#new'
+post '/users' => 'users#create'
+  
+get '/login' => 'sessions#new'
+post '/login' => 'sessions#create'
+delete '/logout' => 'sessions#destroy'
 
-	get '/educations' => 'educations#index'
+get '/educations' => 'educations#index'
+get '/educations/:id' => 'educations#show'
+get '/educations/:id/edit' => 'educations#edit'
+patch '/educations/:id' => 'educations#update' 
 
-	get '/students' => 'students#index'
-	get '/students/:id' => 'students#show'
-	get '/students/:id/edit' => 'students#edit'
-	patch '/students/:id' => 'students#update'
+get '/students' => 'students#index'
+get '/students/:id' => 'students#show'
+get '/students/:id/edit' => 'students#edit'
+patch '/students/:id' => 'students#update'
+
+get '/experiences' => 'experiences#index'
+get '/experiences/new' => 'experiences#new'
+post '/experiences' => 'experiences#create'
+get '/experiences/:id/edit' => 'experirences#edit'
+patch '/experiences/:id' => 'experiences#update'
+get '/experiences/:id' => 'experiences#show'
+delete '/experiences/:id' => 'experiences#destrory'
+
+
 end
