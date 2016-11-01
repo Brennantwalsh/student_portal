fa0971c1803245755225d2a98b08fcd0532923bd
Brennantwalsh <brennanthomaswalsh@gmail.com>
2016-10-31 20:37:17 -0500

    education unirest call working

--- app/models/education.rb
+++ app/models/education.rb
@@ -1,5 +1,6 @@
 class Education
-  attr_reader :id, :start_date, :end_date, :degree, :university_name, :details
+  attr_accessor :start_date, :end_date, :degree, :university_name, :details
+  attr_reader :id, :student_id
 
   def initialize(hash)
     @id = hash["id"]
@@ -8,27 +9,33 @@ class Education
     @degree = hash["degree"]
     @university_name = hash["university_name"]
     @details = hash["details"]
+    @student_id = hash["student_id"]
   end
 
   def self.all 
-    collection = [{"id" => "1", "start_date" => "11-10-19", "end_date" => "11-10-16", "degree" => "Acting", "university_name" => "point park", "details" => "welp" }]
+    collection = []
+      Unirest.get("#{ENV['API_DOMAIN_NAME']}/educations.json", 
+        headers: {"Accept" => "applications/json"}).body.each do |educations_hash|
+        collection << Education.new(educations_hash)
+      end
     collection
   end
 
   def self.find(params_id)
-    Education.new(Unirest.get("#{ENV['API_DOMAIN_NAME']}/api/v1/educations/#{params_id}.json",
-      headers: {"Accept" => "applications/json",
-        }).body)
+    Education.new(Unirest.get("#{ENV['API_DOMAIN_NAME']}/educations/#{params_id}.json",
+      headers: {"Accept" => "applications/json"}).body)
   end
 
-  def self.update
-    Unirest.patch("#{ENV['API_DOMAIN_NAME']}/api/v1/star_wars/#{id}",
+  def update(hash)
+    Unirest.patch("#{ENV['API_DOMAIN_NAME']}/educations/#{id}",
                               headers: {"Accept" => "applications/json",
                               },
                               parameters: {
                                 start_date: hash[:start_date],
                                 end_date: hash[:end_date],
-                                quote: hash[:quote]
+                                degree: hash[:degree],
+                                university_name: hash[:university_name],
+                                details: hash[:hash]
                               })
   end
 
